fd4167c5aead1cc8658d3be3de2a14f4316225cd
Alexander Bazo <alexanderbazo@googlemail.com>
2021-04-28 08:48:14 +0200

    Update starter code

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,15 +3,15 @@
 import Config from "./utils/config.js";
 
 function init() {
-  // Print current game configuration
-  console.log("Current game configuration:");
-  for (let attr in Config) {
-    if (Config.hasOwnProperty(attr)) {
-      console.log(`[${attr}] = ${Config[attr]}`);
+    // Print current game configuration
+    console.log("Current game configuration:");
+    for (let attr in Config) {
+        if (Object.prototype.hasOwnProperty.call(Config, attr)) {
+            console.log(`[${attr}] = ${Config[attr]}`);
+        }
     }
-  }
-  console.log("Initializing application");
-  // Start here
+    console.log("Initializing application");
+    // Start here
 }
 
 init();
