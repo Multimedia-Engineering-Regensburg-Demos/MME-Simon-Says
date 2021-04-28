09f52fa359df5cd4afffc830b16fb5a40f3c82d7
Alexander Bazo <alexanderbazo@googlemail.com>
2021-04-28 09:01:18 +0200

    Refactoring

--- resources/js/index.js
+++ resources/js/index.js
@@ -6,11 +6,7 @@ function init() {
     // Print current game configuration
     console.log("Current game configuration:");
     for (let attr in Config) {
-<<<<<<< HEAD
-        if (Object.prototype.hasOwnProperty.call(Config, attr)) {
-=======
         if (Object.hasOwnProperty.call(Config, attr)) {
->>>>>>> 595a8e377b87a6f583756189c50fa2d055222312
             console.log(`[${attr}] = ${Config[attr]}`);
         }
     }
